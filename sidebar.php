462bd5863c4a593e3204e36c034292ab817ef577
MuhdUmarDarwisy <um4rofficial@gmail.com>
2021-07-23 00:16:21 +0800

    first commit

---
+++ sidebar.php
@@ -0,0 +1,51 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">STEM</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Nav Item - Dashboard -->
+<div class="sidebar-heading">
+    Main
+</div>
+<li class="nav-item active">
+    <a class="nav-link" href="index.php">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+</li>
+
+
+<!-- Nav Item - Charts / Students -->
+<li class="nav-item">
+    <a class="nav-link" href="#">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Students</span></a>
+</li>
+
+<!-- Nav Item - Tables / Logs-->
+<li class="nav-item">
+    <a class="nav-link" href="tables.php">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Logs</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+<!-- Sidebar Message -->
+
+</ul>
+<!-- End of Sidebar -->
